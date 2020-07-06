e09ba7ef8af3bb76047821bfc1a676245c58cd4f
Mercy Bickell <mercycat@fb.com>
2020-07-06 10:35:06 -0600

    create login view controller and add buttons and text fields for making an account

---
+++ ParseChatDemo/View Controllers/LoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.h
+//  ParseChatDemo
+//
+//  Created by Mercy Bickell on 7/6/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ParseChatDemo/View Controllers/LoginViewController.m
@@ -0,0 +1,40 @@
+//
+//  LoginViewController.m
+//  ParseChatDemo
+//
+//  Created by Mercy Bickell on 7/6/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)didPressSignUp:(id)sender {
+}
+
+- (IBAction)didPressLogin:(id)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
