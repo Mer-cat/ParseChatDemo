f82389aea865713ad50b800e3ffe4d19b94fc7ab
Mercy Bickell <mercycat@fb.com>
2020-07-06 11:00:58 -0600

    create chat view controller

---
+++ ParseChatDemo/View Controllers/ChatViewController.h
@@ -0,0 +1,17 @@
+//
+//  ChatViewController.h
+//  ParseChatDemo
+//
+//  Created by Mercy Bickell on 7/6/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChatViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ParseChatDemo/View Controllers/ChatViewController.m
@@ -0,0 +1,31 @@
+//
+//  ChatViewController.m
+//  ParseChatDemo
+//
+//  Created by Mercy Bickell on 7/6/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import "ChatViewController.h"
+
+@interface ChatViewController ()
+
+@end
+
+@implementation ChatViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
